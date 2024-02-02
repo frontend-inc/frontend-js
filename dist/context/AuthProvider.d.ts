7a0d1734b174d8beecf054ccd02b87a2404d7ae3
Rami Bitar <rbitar@gmail.com>
2024-02-02 13:28:49 -0500

    deploy

---
+++ dist/context/AuthProvider.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+declare type AuthProviderProps = {
+    serverPath?: string;
+    children: React.ReactNode;
+};
+declare const AuthProvider: (props: AuthProviderProps) => any;
+export default AuthProvider;
--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,7 @@
 // Common
 export { default as ApiContext } from './ApiContext'
 export { default as ApiProvider } from './ApiProvider'
+export { default as AuthContext } from './AuthContext'
+export { default as AuthProvider } from './AuthProvider'
 export { default as ResourceContext } from './ResourceContext'
 export { default as ResourceProvider } from './ResourceProvider'
