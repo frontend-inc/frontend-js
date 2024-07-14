841292ddd35276fa00207f8b9555b2b71d9af0b0
Rami Bitar <rbitar@gmail.com>
2024-07-14 12:47:34 -0400

    deploy

--- dist/hooks/index.d.ts
+++ dist/hooks/index.d.ts
@@ -1,6 +1,5 @@
 export { default as useApi } from './useApi';
 export { default as useAuth } from './useAuth';
-export { default as useList } from './useList';
 export { default as useDelayedLoading } from './useDelayedLoading';
 export { default as useResource } from './useResource';
 export { default as useResourceContext } from './useResourceContext';
--- dist/hooks/index.js
+++ dist/hooks/index.js
@@ -3,13 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.useLoadingWrapper = exports.useQueryContext = exports.useQuery = exports.useResourceContext = exports.useResource = exports.useDelayedLoading = exports.useList = exports.useAuth = exports.useApi = void 0;
+exports.useLoadingWrapper = exports.useQueryContext = exports.useQuery = exports.useResourceContext = exports.useResource = exports.useDelayedLoading = exports.useAuth = exports.useApi = void 0;
 var useApi_1 = require("./useApi");
 Object.defineProperty(exports, "useApi", { enumerable: true, get: function () { return __importDefault(useApi_1).default; } });
 var useAuth_1 = require("./useAuth");
 Object.defineProperty(exports, "useAuth", { enumerable: true, get: function () { return __importDefault(useAuth_1).default; } });
-var useList_1 = require("./useList");
-Object.defineProperty(exports, "useList", { enumerable: true, get: function () { return __importDefault(useList_1).default; } });
 var useDelayedLoading_1 = require("./useDelayedLoading");
 Object.defineProperty(exports, "useDelayedLoading", { enumerable: true, get: function () { return __importDefault(useDelayedLoading_1).default; } });
 var useResource_1 = require("./useResource");
